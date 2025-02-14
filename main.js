e91dce88fc963eed2ff9535ba69bbf17e92aced8
João Vitor <134158096+joaovitorpapalardo@users.noreply.github.com>
2025-02-14 10:50:37 -0300

    Update main.js
    Adicionando ScrollReveal no JS.

---
+++ main.js
@@ -0,0 +1,104 @@
+window.revelar = ScrollReveal({reset:true})
+
+//principal
+revelar.reveal('.texto-principal', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+
+})
+
+revelar.reveal('.link-sobre-mim', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom'
+
+})
+
+revelar.reveal('.imagem-eu', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right'
+
+})
+
+// Sobre
+
+revelar.reveal('.efeito-txt-dev', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+
+})
+
+revelar.reveal('.efeito-txt-sobre', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-txt-skills', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'top'
+})
+
+revelar.reveal('.efeito-txt-skills-1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-txt-skills-2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-txt-skills-3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-txt-skills-4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-txt-skills-5', {
+    duration: 2000,
+    distance: '90px',
+    delay: 600
+})
+
+revelar.reveal('.efeito-txt-skills-6', {
+    duration: 2000,
+    distance: '90px',
+    delay: 700
+})
+
+revelar.reveal('.efeito-txt-projetos', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-txt-projetos-1', {
+    duration: 2000,
+    distance: '120px',
+    delay: 200,
+})
+
+revelar.reveal('.efeito-txt-projetos-2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 400,
+})
+
+revelar.reveal('.efeito-txt-projetos-3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 600,
+})
+
